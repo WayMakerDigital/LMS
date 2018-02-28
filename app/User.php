1345f9bbae61af8669e3a41d9304a691f36a4543
Awonugba Isaac Tolu <isaacawonugba@yahoo.com>
2018-02-28 15:15:17 +0100

    Add files via upload

--- app/User.php
+++ app/User.php
@@ -2,13 +2,14 @@
 
 namespace App;
 
+use Laravel\Cashier\Billable;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, Billable;
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'username', 'email', 'password', 'confirm_password',
+        'first_name', 'last_name', 'username', 'email', 'password', 'confirm_password', 'stripe_id',
     ];
 
     /**
