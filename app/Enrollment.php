41cac90de487b1a88f8bb868d039b41ca7f6aae7
Awonugba Isaac Tolu <isaacawonugba@yahoo.com>
2018-02-28 15:33:21 +0100

    Add files via upload

--- app/Enrollment.php
+++ app/Enrollment.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
-    //
+    protected $fillable = [
+        'stripe_id', 'subscription_id', 'course_id', 'ends_at', 
+    ];
 }
